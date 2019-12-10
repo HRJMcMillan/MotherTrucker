0122ca9f0f5d9b82f30ee8f7cb154d1618fbab47
Phil Davidson <pxdavidson@msn.com>
2019-12-10 21:11:57 +0000

    Added test scene for procedural rolling road

---
+++ MotherTrucker/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+    float moveSpeed = 5;
+
+    // Cache
+    Transform playerTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        playerTransform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
