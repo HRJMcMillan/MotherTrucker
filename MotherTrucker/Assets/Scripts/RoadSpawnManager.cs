6a9eec8761b619c7eda162e891f2b0ca920cc561
Phil Davidson <pxdavidson@msn.com>
2019-12-11 00:35:02 +0000

    Added RoadSpawner and RoadSpawnerManager
    The roads now correctly spawn from an array

---
+++ MotherTrucker/Assets/Scripts/RoadSpawnManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawnManager : MonoBehaviour
+{
+    // Variables
+    [SerializeField] GameObject[] roadSections;
+
+    public GameObject GetRoad()
+    {
+        GameObject nextSection = roadSections[Random.Range(0, roadSections.Length)];
+        return nextSection;
+    }
+}
---
+++ MotherTrucker/Assets/Scripts/RoadSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour
+{
+    // Cache
+    RoadSpawnManager roadSpawnManager;
+
+    private void OnTriggerEnter(Collider collided)
+    {
+        Transform spawnLocation = transform;
+        GameObject nextSection = Instantiate(roadSpawnManager.GetRoad(), spawnLocation.position, transform.rotation);
+        print(nextSection.name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roadSpawnManager = FindObjectOfType<RoadSpawnManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
