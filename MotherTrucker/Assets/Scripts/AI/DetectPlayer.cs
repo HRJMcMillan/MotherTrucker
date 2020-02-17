3346b783e8dfbf46bcac2034568c79ba3d5477e8
Phil Davidson <pxdavidson@msn.com>
2020-02-17 21:03:41 +0000

    Added placeholder NPC
    Added DetectPlayer.cs

---
+++ MotherTrucker/Assets/Scripts/AI/DetectPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    // Detects trigger colliders that intersect it and verifies if it's the player
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = GameObject.FindWithTag("Player");
+        if (other.gameObject == player)
+        {
+            InteractWithPlayer();
+        }
+        else return;
+    }
+
+    private void InteractWithPlayer()
+    {
+        
+    }
+}
